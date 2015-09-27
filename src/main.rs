e4a75bfb67b2c27e936d054e9360348506129627
Sam Merrell <merrell.sam@gmail.com>
2015-09-26 22:18:23 -0600

    Print test lines

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,13 @@ mod tap;
 // flag to disable colorized output
 fn main() {
     let stdin = io::stdin();
-    let mut iter = stdin.lock().lines();
-
-    // this is probably bad, bad, bad
-    let version_line = &iter.next().unwrap().unwrap();
-    let version = tap::read_version(&version_line);
-
-    let mut parser = tap::TapHarness::new(version);
+    let iter = stdin.lock().lines();
+    let mut parser = tap::TapHarness::new(tap::TapVersion::Thirteen);
 
     for line_res in iter {
         let line = line_res.unwrap();
         parser.read_line(&line.trim());
+        println!("{}", &line.trim());
     }
 
     println!("{}", &parser.summarize());
