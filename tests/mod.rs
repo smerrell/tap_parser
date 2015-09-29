3c96868a2ab37d8a296fd87252a348422c5b492f
Sam Merrell <merrell.sam@gmail.com>
2015-09-29 11:34:46 -0600

    Try hamcrest for test assertion matching

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,6 +1,9 @@
+#[cfg(test)]
+extern crate hamcrest;
 extern crate tap_parser;
 
 use tap_parser::tap::{TapHarness,TapVersion};
+use hamcrest::{assert_that,equal_to,is};
 
 #[test]
 pub fn read_line_for_failed_test_returns_passed_false() {
@@ -9,9 +12,10 @@ pub fn read_line_for_failed_test_returns_passed_false() {
 not ok Test something broken";
     let mut parser = TapHarness::new(TapVersion::Thirteen);
     let mut lines = input.lines();
+
     parser.read_line(&lines.next().unwrap());
     let result = parser.read_line(&lines.next().unwrap());
 
     println!("{:?}", result);
-    assert_eq!(result.unwrap().passed, false);
+    assert_that(result.unwrap().passed, is(equal_to(false)));
 }
